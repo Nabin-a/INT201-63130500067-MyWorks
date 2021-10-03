bd09a2c0f79958c62c9cb00af0dcb7fe56a6261c
COD-Low <68808823+Nabin-a@users.noreply.github.com>
2021-10-04 01:14:19 +0700

    modified test2.js v.1

--- selfPractice/test2.js
+++ selfPractice/test2.js
@@ -1,1 +1,6 @@
 console.log('5'*'4');
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();        // First sort the elements of fruits
+fruits.reverse();     // Then reverse the order of the elements
+console.log(fruits);
